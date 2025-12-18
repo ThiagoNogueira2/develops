85007d132d6330c51e27b14542f4ecae8433c18f
ThiagoNogueira2 <thiagonogueira_2003@hotmail.com>
2025-12-18 08:15:12 -0300

    feat: Inserindo Header ao sistema

--- src/modules/Topo/Header.vue
+++ src/modules/Topo/Header.vue
@@ -1,11 +1,15 @@
 <template>
-  <header>
-    <!-- Conteúdo do Topo -->
-
-    <h1>Testando1</h1>
+  <header class="header bg-[#076633] p-2 md:p-4">
+    <div class="container-1170">
+      <div class="flex flex-wrap justify-center md:justify-around gap-2 text-gray-300 text-sm md:text-base">
+        <p>Avenida Cerro Azul - Jardim Horizonte</p>
+        <p>(44) 9 9909-0909</p>
+        <p>suporte@ingadigital.com.br</p>
+      </div>
+      <div class="border-t border-gray-300 mt-2 md:mt-3"></div>
+    </div>
   </header>
 </template>
 
 <script setup lang="ts">
-// Lógica do componente Topo
 </script>
