a43316ffff84291c609d875467c816da0bb039bb
ThiagoNogueira2 <thiagonogueira_2003@hotmail.com>
2025-12-22 13:47:20 -0300

    feat: Atualizando Header para informações coerentes

--- src/modules/Topo/Header.vue
+++ src/modules/Topo/Header.vue
@@ -4,7 +4,7 @@
       class="w-full max-w-[1170px] mx-auto flex flex-wrap justify-around text-white text-md md:text-sm p-4"
     >
       <p>Avenida Cerro Azul - Jardim Horizonte</p>
-      <p>(44)99990-9990</p>
+      <p>(44)99940-9990</p>
       <p>suporte@ingadigital.com.br</p>
 
       <div class="w-full h-px mt-4 bg-white/60"></div>
